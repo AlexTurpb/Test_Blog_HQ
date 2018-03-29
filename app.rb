aac9bfbc5f28d860a46c6d2fdd7ca26f60eef1c0
Alex_Turpb <turpb41@gmail.com>
2018-03-30 02:21:09 +0300

    one-to-many association set, schema update

--- app.rb
+++ app.rb
@@ -7,11 +7,11 @@ require 'sinatra/activerecord'
 set :database, 'sqlite3:blog_hq.db'
 
 class Post < ActiveRecord::Base
-	has_many :comments
-end
+	has_many :comments, :foreing_key => "postID"
+ end
 
 class Comment < ActiveRecord::Base
-	belongs_to :posts
+	belongs_to :posts, :foreing_key => "postID"
 end
 
 get '/' do
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180329105559) do
+ActiveRecord::Schema.define(version: 20180329213055) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "comment"
+    t.string "author"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "post"
