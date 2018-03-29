94852752641f70a6ac61b6dcec0333838415d8d7
Alex_Turpb <turpb41@gmail.com>
2018-03-29 14:45:37 +0300

    create entities and migration

--- app.rb
+++ app.rb
@@ -6,6 +6,13 @@ require 'sinatra/activerecord'
 
 set :database, 'sqlite3:blog_hq.db'
 
+class Post < ActiveRecord::Base
+	has_many :comments
+end
+
+class Comment < ActiveRecord::Base
+end
+
 get '/' do
 	erb "Hello! <a href=\"https://github.com/bootstrap-ruby/sinatra-bootstrap\">Original</a> pattern has been modified for <a href=\"http://rubyschool.us/\">Ruby School</a>"			
 end
