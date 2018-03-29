9f742458524decc6232b70a0881ec30c90136c37
Alex_Turpb <turpb41@gmail.com>
2018-03-30 02:28:14 +0300

    :foreign_key bug fixed

--- app.rb
+++ app.rb
@@ -7,11 +7,11 @@ require 'sinatra/activerecord'
 set :database, 'sqlite3:blog_hq.db'
 
 class Post < ActiveRecord::Base
-	has_many :comments, :foreing_key => "postID"
+	has_many :comments, :foreign_key => "postID"
  end
 
 class Comment < ActiveRecord::Base
-	belongs_to :posts, :foreing_key => "postID"
+	belongs_to :posts, :foreign_key => "postID"
 end
 
 
